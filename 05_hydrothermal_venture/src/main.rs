9768bf86a1037575cf6969695b3eb9242bddc283
Bethany Janos <bethanyjanos28@gmail.com>
2021-12-05 11:09:08 -0500

    Break into function

--- 05_hydrothermal_venture/src/main.rs
+++ 05_hydrothermal_venture/src/main.rs
@@ -4,51 +4,14 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    println!("part 1");
-    /*
-    let mut grid: HashMap<String, i32> = HashMap::new();
-    if let Ok(lines) = read_lines("./hydrothermal_venture.txt") {
-        for line in lines {
-            if let Ok(segment) = line {
-                let mut points = segment.split(" -> ");
-                let point_1: Vec<i32> = points.next().unwrap().split(",").map(|p| p.parse::<i32>().unwrap()).collect();
-                let point_2: Vec<i32> = points.next().unwrap().split(",").map(|p| p.parse::<i32>().unwrap()).collect();
-                if point_1[1] != point_2[1] && point_1[0] != point_2[0] {
-                    continue;
-                }
-                let mut start = &point_1;
-                let mut end = &point_2;
-                if point_1[1] == point_2[1] {
-                    if point_1[0] > point_2[0] {
-                        start = &point_2;
-                        end = &point_1
-                    }
-                } else {
-                    if point_1[1] > point_2[1] {
-                        start = &point_2;
-                        end = &point_1;
-                    }
-                }
-                //println!("segment {:?} -> {:?}", start, end);
-                for y in start[1]..=end[1] {
-                    for x in start[0]..=end[0] {
-                        //println!("point x = {}, y = {}", x, y);
-                        let count = grid.entry(format!("{}:{}", x, y)).or_insert(0);
-                        *count += 1;
-                    }
-                }
-            }
-        }
-    }
+    let file_name = "./hydrothermal_venture.txt";
+    println!("part 1: {}", find_danger_points(false, file_name));
+    println!("part 2: {}", find_danger_points(true, file_name));
+}
 
-    //println!("Grid: {:?}", grid);
-
-    let danger_points: Vec<i32> = grid.into_values().filter(|count| count >= &2).collect();
-    println!("Danger points count: {}", danger_points.len());
-    */
-    println!("part 2");
+fn find_danger_points(track_diagonals: bool, file_name: &str) -> usize {
     let mut grid: HashMap<String, i32> = HashMap::new();
-    if let Ok(lines) = read_lines("./hydrothermal_venture.txt") {
+    if let Ok(lines) = read_lines(file_name) {
         for line in lines {
             if let Ok(segment) = line {
                 let mut points = segment.split(" -> ");
@@ -65,11 +28,12 @@ fn main() {
                     .map(|p| p.parse::<i32>().unwrap())
                     .collect();
 
-                //println!("segment {:?} -> {:?}", start, end);
                 let (x1, y1, x2, y2) = (start[0], start[1], end[0], end[1]);
                 let (mut x, mut y) = (x1, y1);
+                if !track_diagonals && (x1 != x2 && y1 != y2) {
+                    continue;
+                }
                 while x != x2 || y != y2 {
-                    //println!("x = {}, y = {}", x, y);
                     let count = grid.entry(format!("{}:{}", x, y)).or_insert(0);
                     *count += 1;
 
@@ -96,11 +60,8 @@ fn main() {
             }
         }
     }
-
-    //println!("Grid: {:?}", grid);
-
     let danger_points: Vec<i32> = grid.into_values().filter(|count| count >= &2).collect();
-    println!("Danger points count: {}", danger_points.len());
+    return danger_points.len();
 }
 
 // The output is wrapped in a Result to allow matching on errors

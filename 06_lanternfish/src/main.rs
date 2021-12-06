5166ccbbde40ba50f85ca5484dd955be80de2f7c
Bethany Janos <bethanyjanos28@gmail.com>
2021-12-06 08:38:13 -0500

    checkpoint

--- 06_lanternfish/src/main.rs
+++ 06_lanternfish/src/main.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 
 fn main() {
     let mut school: Vec<i32> = Vec::new();
-    let days = 256;
+    let days = 80;
     if let Ok(lines) = read_lines("./lanternfish_simple.txt") {
         for line in lines {
             if let Ok(initial_fish) = line {
@@ -15,8 +15,7 @@ fn main() {
         }
     }
 
-    for day in 0..days {
-        println!("day {}", day);
+    for _ in 0..days {
         let mut new_fish = 0;
         for i in 0..school.len() {
             if school[i] == 0 {
@@ -32,7 +31,28 @@ fn main() {
         }
     }
 
-    println!("School size: {}", school.len());
+    println!("part 1\nSchool size: {}", school.len());
+
+    school.clear();
+    if let Ok(lines) = read_lines("./lanternfish_simple.txt") {
+        for line in lines {
+            if let Ok(initial_fish) = line {
+                for timer in initial_fish.split(",") {
+                    school.push(timer.parse::<i32>().unwrap());
+                }
+            }
+        }
+    }
+
+    let total_days: u32 = 256;
+    let mut fish_count: u128 = 0;
+    let base: u128 = 2;
+    for fish in school {
+        println!("fish count: {}", fish_count);
+        fish_count += base.pow((total_days - fish as u32)/6);
+    }
+
+    println!("part 2\nSchool size: {}", fish_count);
 }
 
 // The output is wrapped in a Result to allow matching on errors

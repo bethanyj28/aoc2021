af1d3dc56b391f85ed216f3820e4395b60526255
Bethany Janos <bjanos@redventures.com>
2021-12-20 18:39:54 -0500

    oof

--- 16_packet_decoder/src/main.rs
+++ 16_packet_decoder/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::cmp;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -23,7 +24,7 @@ fn main() {
         (String::from("F"), String::from("1111")),
     ]);
     let mut binary: Vec<String> = Vec::new();
-    if let Ok(lines) = read_lines("./packet_decoder_simple.txt") {
+    if let Ok(lines) = read_lines("./packet_decoder.txt") {
         for line in lines {
             if let Ok(hex) = line {
                 let parts: Vec<String> = hex.split("").map(|p| p.to_string()).collect();
@@ -31,12 +32,283 @@ fn main() {
                     if part == "" {
                         continue;
                     }
-                    let val = translate_hex.get(&part).unwrap();
-                    binary.append(val.to_mut_vec()); 
+                    let mut val: Vec<String> = translate_hex.get(&part).unwrap().chars().map(|b| b.to_string()).collect();
+                    binary.append(&mut val);
                 }
             }
         }
     }
+
+    //println!("version sum: {}", sum_packet_version(binary.clone()));
+    let s = order(binary, Vec::new());
+    println!("order: {:?}", s);
+    //println!("evaluated: {}", evaluate(s));
+}
+
+fn evaluate(mut s: Vec<String>) -> u32 {
+    let mut buffer: Vec<u32> = Vec::new();
+    while !s.is_empty() {
+        println!("s: {:?}", s);
+        println!("b: {:?}", buffer);
+        let op = s.pop().unwrap();
+        match &*op {
+            "+" => {
+                let mut val = 0;
+                while !buffer.is_empty() {
+                    val += buffer.pop().unwrap();
+                }
+                buffer.push(val);
+            },
+            "*" => {
+                let mut val = 1;
+                while !buffer.is_empty() {
+                    val *= buffer.pop().unwrap();
+                }
+                buffer.push(val);
+            },
+            "min" => {
+                let mut val = u32::MAX;
+                while !buffer.is_empty() {
+                    let num = buffer.pop().unwrap();
+                    if num < val {
+                        val = num;
+                    }
+                }
+                buffer.push(val);
+            },
+            "max" => {
+                let mut val = 0;
+                while !buffer.is_empty() {
+                    let num = buffer.pop().unwrap();
+                    if num < val {
+                        val = num;
+                    }
+                }
+                buffer.push(val);
+            },
+            "gt" => {
+                if buffer.pop().unwrap() < buffer.pop().unwrap() {
+                    buffer.push(1);
+                } else {
+                    buffer.push(0);
+                }
+            },
+            "lt" => {
+                if buffer.pop().unwrap() > buffer.pop().unwrap() {
+                    buffer.push(1);
+                } else {
+                    buffer.push(0);
+                }
+            },
+            "eq" => {
+                if buffer.pop().unwrap() == buffer.pop().unwrap() {
+                    buffer.push(1);
+                } else {
+                    buffer.push(0);
+                }
+            },
+            _ => {
+                let num = u32::from_str_radix(&op, 2).unwrap();
+                buffer.push(num);
+            },
+        }
+    }
+
+    return buffer.pop().unwrap();
+}
+
+fn order(b: Vec<String>, mut s: Vec<String>) -> Vec<String> {
+     if b.len() <= 10 {
+        return s;
+    }
+    let (prefix, mut rest) = b.split_at(6);
+    // second 3 = type_id
+    let type_id_bin = prefix[3..].join("");
+    let type_id = u32::from_str_radix(&type_id_bin, 2).unwrap();
+
+    // type = 4 -> number, no other segments
+    if type_id == 4 {
+        println!("num packet");
+        let mut num_bin = String::from("");
+        loop {
+            let (num_bits, temp) = rest.split_at(5);
+            rest = temp;
+            num_bin.push_str(&*num_bits[1..].join(""));
+            if num_bits[0] == "0" {
+                break;
+            }
+        }
+
+        let num = u32::from_str_radix(&num_bin, 2).unwrap();
+        println!("num: {}", num);
+        s.push(format!("{}", num));
+        if rest.len() <= 10 {
+            return s;
+        }
+
+        return order(rest.into_iter().cloned().collect(), s);
+    }
+    println!("operator packet");
+    let mut exp_op: String = String::from("");
+    match type_id {
+        0 => exp_op = String::from("+"),
+        1 => exp_op = String::from("*"),
+        2 => exp_op = String::from("min"),
+        3 => exp_op = String::from("max"),
+        5 => exp_op = String::from("gt"),
+        6 => exp_op = String::from("lt"),
+        7 => exp_op = String::from("eq"),
+        _ => unreachable!(),
+    }
+    s.push(exp_op);
+
+    // length type id
+    let mut rest_vec: Vec<String> = rest.into_iter().cloned().collect();
+    let lt_id = rest_vec.remove(0);
+    if lt_id == "0" { // 15 bits -> total length of subpackets
+        let (_, temp) = rest_vec.split_at(15);
+        rest_vec = temp.into_iter().cloned().collect();
+    } else {
+        let (_, temp) = rest_vec.split_at(11);
+        rest_vec = temp.into_iter().cloned().collect();
+    }
+
+    return order(rest_vec, s);
+}
+
+/*
+fn evaluate(b: Vec<String>, op: String) -> u32 {
+    if b.len() <= 10 {
+        return 0;
+    }
+    let (prefix, mut rest) = b.split_at(6);
+    // second 3 = type_id
+    let type_id_bin = prefix[3..].join("");
+    let type_id = u32::from_str_radix(&type_id_bin, 2).unwrap();
+
+    println!("b: {:?}\nop: {}\nt_id: {}", b, op, type_id);
+    // type = 4 -> number, no other segments
+    let mut exp_op: String = String::from("");
+    if type_id == 4 {
+        println!("num packet");
+        let mut num_bin = String::from("");
+        loop {
+            let (num_bits, temp) = rest.split_at(5);
+            rest = temp;
+            num_bin.push_str(&*num_bits[1..].join(""));
+            if num_bits[0] == "0" {
+                break;
+            }
+        }
+        let num = u32::from_str_radix(&num_bin, 2).unwrap();
+        if rest.len() <= 10 {
+            return num;
+        }
+        match &*op {
+            "+" => {
+                return num + evaluate(rest.into_iter().cloned().collect(), op);
+            },
+            "*" => {
+                return num * evaluate(rest.into_iter().cloned().collect(), op);
+            },
+            "min" => {
+                return cmp::min(num, evaluate(rest.into_iter().cloned().collect(), op));
+            },
+            "max" => {
+                return cmp::max(num, evaluate(rest.into_iter().cloned().collect(), op));
+            },
+            "gt" => {
+                if num > evaluate(rest.into_iter().cloned().collect(), exp_op){
+                    return 1;
+                }
+                return 0;
+            },
+            "lt" => {
+                if num < evaluate(rest.into_iter().cloned().collect(), exp_op){
+                    return 1;
+                }
+                return 0;
+            },
+            "eq" => {
+                if num == evaluate(rest.into_iter().cloned().collect(), exp_op){
+                    return 1;
+                }
+                return 0;
+            },
+            _ => {
+                return num;
+            },
+        }
+    }
+    println!("operator packet");
+    match type_id {
+        0 => exp_op = String::from("+"),
+        1 => exp_op = String::from("*"),
+        2 => exp_op = String::from("min"),
+        3 => exp_op = String::from("max"),
+        5 => exp_op = String::from("gt"),
+        6 => exp_op = String::from("lt"),
+        7 => exp_op = String::from("eq"),
+        _ => unreachable!(),
+    }
+
+    // length type id
+    let mut rest_vec: Vec<String> = rest.into_iter().cloned().collect();
+    let lt_id = rest_vec.remove(0);
+    if lt_id == "0" { // 15 bits -> total length of subpackets
+        let (_, temp) = rest_vec.split_at(15);
+        rest_vec = temp.into_iter().cloned().collect();
+    } else {
+        let (_, temp) = rest_vec.split_at(11);
+        rest_vec = temp.into_iter().cloned().collect();
+    }
+
+    return evaluate(rest_vec, exp_op);
+}
+*/
+
+fn sum_packet_version(b: Vec<String>) -> u32 {
+    if b.len() <= 10 {
+        return 0;
+    }
+    let (prefix, mut rest) = b.split_at(6);
+    // first 3 = version
+    let version_bin = prefix[..3].join("");
+    let version = u32::from_str_radix(&version_bin, 2).unwrap();
+    // second 3 = type_id
+    let type_id_bin = prefix[3..].join("");
+    let type_id = u32::from_str_radix(&type_id_bin, 2).unwrap();
+
+    // type = 4 -> number, no other segments
+    if type_id == 4 {
+        println!("num packet");
+        loop {
+            let (num, temp) = rest.split_at(5);
+            rest = temp;
+            if num[0] == "0" {
+                break;
+            }
+        }
+        if rest.len() <= 10 {
+            return version;
+        }
+
+        return version + sum_packet_version(rest.into_iter().cloned().collect());
+    }
+    println!("operator packet");
+
+    // length type id
+    let mut rest_vec: Vec<String> = rest.into_iter().cloned().collect();
+    let lt_id = rest_vec.remove(0);
+    if lt_id == "0" { // 15 bits -> total length of subpackets
+        let (_, temp) = rest_vec.split_at(15);
+        rest_vec = temp.into_iter().cloned().collect();
+    } else {
+        let (_, temp) = rest_vec.split_at(11);
+        rest_vec = temp.into_iter().cloned().collect();
+    }
+
+    return version + sum_packet_version(rest_vec);
 }
 
 
